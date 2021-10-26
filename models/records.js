021c0c7f461367772595f3c36cfb5c9dd3509a47
smilingfroggy <smilingfroggy@hotmail.com>
2021-10-26 17:55:40 +0800

    feat: add models of users, records, and categories

---
+++ models/records.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const recordsSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+    default: Date.now()
+  },
+  amount: {
+    type: Number,
+    required: true
+  },
+  categoryId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Categories',
+    required: true
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Users',
+    required: true,
+    index: true
+  }
+})
+
+module.exports = mongoose.model('Records', recordsSchema)
