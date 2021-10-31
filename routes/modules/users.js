3bb338da8965a27226cab4b3afd78a53571740d0
smilingfroggy <smilingfroggy@hotmail.com>
2021-11-01 03:51:56 +0800

    feat: add register page and routes

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -10,4 +10,31 @@ router.post('/login', (req, res) => {
   res.redirect('/')
 })
 
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  Users.findOne({ email })
+    .lean()
+    .then(user => {
+      console.log(user)
+      if (user) { //檢查email是否重複註冊
+        console.log('此email已被註冊')
+        return res.render('register', { name, email, password, confirmPassword })
+      } else if (password !== confirmPassword) {
+        //檢查輸入密碼是否相同
+        console.log('請檢查兩次輸入的密碼')
+        return res.render('register', { name, email, password, confirmPassword })
+      } else {
+        Users.create({ name, email, password })
+          .then(() => {
+            res.redirect('/')
+            console.log(`name: ${name} registered`)
+          })
+      }
+    })
+})
+
 module.exports = router
