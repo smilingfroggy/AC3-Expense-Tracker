53d88848a3e572415cab7c1a6e341dd101f275bc
smilingfroggy <smilingfroggy@hotmail.com>
2021-11-01 15:36:12 +0800

    feat: add user authentication middleware in todo and home routers

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
+const { authenticator } = require('../middleware/auth')
 const records = require('./modules/records')
 const home = require('./modules/home')
 const users = require('./modules/users')
 
-router.use('/', home)
-router.use('/records', records)
+router.use('/records', authenticator, records)
 router.use('/users', users)
+router.use('/', authenticator, home)
 
 module.exports = router
