c7d822777eb8034129170ae53018aaf8fbd87101
smilingfroggy <smilingfroggy@hotmail.com>
2021-11-23 15:55:23 +0800

    Add categories selection feature

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,6 +3,13 @@ const router = express.Router()
 const Categories = require('../../models/categories')
 const Records = require('../../models/records')
 const UserId = require('../../models/users')
+const CATEGORY = {
+  "家居物業": '<i class="fas fa-home"></i>',
+  "交通出行": '<i class="fas fa-shuttle-van"></i>',
+  "休閒娛樂": '<i class="fas fa-grin-beam"></i>',
+  "餐飲食品": '<i class="fas fa-utensils"></i>',
+  "其他": '<i class="fas fa-pen"></i>'
+}
 
 router.get('/', (req, res) => {
   const userId = req.user._id
@@ -16,14 +23,31 @@ router.get('/', (req, res) => {
           for (let i = 0; i < records.length; i++) {
             totalAmount += records[i].amount
           }
-          const CATEGORY = {
-            "家居物業": '<i class="fas fa-home"></i>',
-            "交通出行": '<i class="fas fa-shuttle-van"></i>',
-            "休閒娛樂": '<i class="fas fa-grin-beam"></i>',
-            "餐飲食品": '<i class="fas fa-utensils"></i>',
-            "其他": '<i class="fas fa-pen"></i>'
-          }
           res.render('index', { categories, records, totalAmount, CATEGORY })
+        })
+    })
+})
+
+router.put('/category', (req, res) => {
+  const userId = req.user._id
+  const categorySelected = req.body.categorySelected  // e.g."家居物業"
+  Categories.find()
+    .lean()
+    .then(categories => {
+      Categories.findOne({ name: categorySelected })
+        .lean()
+        .then(category => {
+          const categoryId = category._id.toString()
+          Records.find({ userId, categoryId })
+            .lean()
+            .then(records => {
+              let totalAmount = 0
+              for (let i = 0; i < records.length; i++) {
+                totalAmount += records[i].amount
+              }
+              res.render('index', { categories, records, totalAmount, CATEGORY })
+            })
+            .catch(err => console.log(err))
         })
     })
 })
